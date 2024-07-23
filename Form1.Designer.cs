d577802d397873f4afaa10d3a431fbe8cb4ee595
Alex Mizell <alexmizell@gmail.com>
2024-07-23 08:48:51 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿using System;
+
+namespace WhisperX_ListenerTest
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnListen = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.cbMomentary = new System.Windows.Forms.CheckBox();
+            this.comboInputs = new System.Windows.Forms.ComboBox();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.progressBarAudioLevel = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textOutput
+            // 
+            this.textOutput.Location = new System.Drawing.Point(512, 66);
+            this.textOutput.Margin = new System.Windows.Forms.Padding(6);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.Size = new System.Drawing.Size(908, 730);
+            this.textOutput.TabIndex = 0;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(507, 37);
+            this.lblOutput.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(71, 25);
+            this.lblOutput.TabIndex = 1;
+            this.lblOutput.Text = "Output";
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(112, 596);
+            this.btnListen.Margin = new System.Windows.Forms.Padding(6);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(279, 87);
+            this.btnListen.TabIndex = 2;
+            this.btnListen.Text = "Listen";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Visible = false;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(112, 695);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(6);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(279, 87);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Visible = false;
+            // 
+            // cbMomentary
+            // 
+            this.cbMomentary.Appearance = System.Windows.Forms.Appearance.Button;
+            this.cbMomentary.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cbMomentary.Location = new System.Drawing.Point(112, 189);
+            this.cbMomentary.Margin = new System.Windows.Forms.Padding(6);
+            this.cbMomentary.Name = "cbMomentary";
+            this.cbMomentary.Size = new System.Drawing.Size(279, 73);
+            this.cbMomentary.TabIndex = 3;
+            this.cbMomentary.Text = "Listen";
+            this.cbMomentary.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cbMomentary.UseVisualStyleBackColor = true;
+            // 
+            // comboInputs
+            // 
+            this.comboInputs.FormattingEnabled = true;
+            this.comboInputs.Location = new System.Drawing.Point(28, 68);
+            this.comboInputs.Margin = new System.Windows.Forms.Padding(6);
+            this.comboInputs.Name = "comboInputs";
+            this.comboInputs.Size = new System.Drawing.Size(447, 32);
+            this.comboInputs.TabIndex = 4;
+            this.comboInputs.SelectedIndexChanged += new System.EventHandler(this.comboInputs_SelectedIndexChanged);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(38, 37);
+            this.lblInput.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(55, 25);
+            this.lblInput.TabIndex = 1;
+            this.lblInput.Text = "Input";
+            // 
+            // progressBarAudioLevel
+            // 
+            this.progressBarAudioLevel.Location = new System.Drawing.Point(28, 119);
+            this.progressBarAudioLevel.Maximum = 32768;
+            this.progressBarAudioLevel.Name = "progressBarAudioLevel";
+            this.progressBarAudioLevel.Size = new System.Drawing.Size(447, 35);
+            this.progressBarAudioLevel.TabIndex = 5;
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1438, 824);
+            this.Controls.Add(this.progressBarAudioLevel);
+            this.Controls.Add(this.comboInputs);
+            this.Controls.Add(this.cbMomentary);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnListen);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.textOutput);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "formMain";
+            this.Text = "WhisperX Listener";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void comboInputs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textOutput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.CheckBox cbMomentary;
+        private System.Windows.Forms.ComboBox comboInputs;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.ProgressBar progressBarAudioLevel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+
+namespace WhisperX_ListenerTest
+{
+    public partial class formMain : Form
+    {
+        private WaveInEvent waveIn;
+        private Timer flashTimer;
+        private bool isListening = false;
+        private WaveFileWriter waveFileWriter;
+        private int fileCount = 0;
+
+        public formMain()
+        {
+            InitializeComponent();
+
+            PopulateAudioInputs(comboInputs);
+
+            flashTimer = new Timer
+            {
+                Interval = 500 // 0.5 seconds
+            };
+            flashTimer.Tick += FlashTimer_Tick;
+
+            cbMomentary.Appearance = Appearance.Button;
+            cbMomentary.MouseDown += cbMomentary_MouseDown;
+            cbMomentary.MouseUp += cbMomentary_MouseUp;
+            cbMomentary.FlatStyle = FlatStyle.Flat; // Ensure it visually behaves like a button
+        }
+
+        private void StartListening()
+        {
+            if (isListening) return;
+
+            waveIn = new WaveInEvent
+            {
+                DeviceNumber = comboInputs.SelectedIndex,
+                WaveFormat = new WaveFormat(16000, 16, 1)
+            };
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+            waveIn.RecordingStopped += WaveIn_RecordingStopped;
+            waveIn.StartRecording();
+
+            // Generate a new file name and create a WaveFileWriter instance
+            string fileName = GenerateFileName();
+            waveFileWriter = new WaveFileWriter(fileName, waveIn.WaveFormat);
+
+            isListening = true;
+            flashTimer.Start();
+        }
+        private void StopListening()
+        {
+            waveIn?.StopRecording();
+            isListening = false;
+            flashTimer.Stop();
+            cbMomentary.BackColor = default;
+
+            // Finalize and close the WaveFileWriter
+            if (waveFileWriter != null)
+            {
+                string fullPath = waveFileWriter.Filename;
+                waveFileWriter.Dispose();
+                waveFileWriter = null;
+
+                // After closing the file, run the WhisperX command
+                RunWhisperX(fullPath);
+            }
+        }
+
+        private void FlashTimer_Tick(object sender, EventArgs e)
+        {
+            cbMomentary.BackColor = cbMomentary.BackColor == Color.Red ? default : Color.Red;
+        }
+
+        private void cbMomentary_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                StartListening();
+            }
+        }
+
+        private void cbMomentary_MouseUp(object sender, MouseEventArgs e)
+        {
+            StopListening();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Space)
+            {
+                if (!isListening)
+                {
+                    StartListening();
+                }
+                else
+                {
+                    StopListening();
+                }
+                return true; // Indicate that the key press has been handled
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        public void PopulateAudioInputs(ComboBox comboInputs)
+        {
+            var enumerator = new MMDeviceEnumerator();
+            int selectedIndex = 0;
+
+            foreach (var endpoint in enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active))
+            {
+                comboInputs.Items.Add(endpoint.FriendlyName);
+                if (endpoint.FriendlyName.IndexOf("USB2.0", StringComparison.OrdinalIgnoreCase) >= 0 && selectedIndex == 0)
+                {
+                    selectedIndex = comboInputs.Items.Count - 1;
+                }
+            }
+
+            if (comboInputs.Items.Count > 0)
+            {
+                comboInputs.SelectedIndex = selectedIndex;
+            }
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (waveFileWriter != null)
+            {
+                waveFileWriter.Write(e.Buffer, 0, e.BytesRecorded);
+                waveFileWriter.Flush();
+            }
+
+            int max = 0;
+            for (int index = 0; index < e.BytesRecorded; index += 2)
+            {
+                short sample = (short)((e.Buffer[index + 1] << 8) | e.Buffer[index]);
+                int absSample = Math.Abs((int)sample);
+                if (absSample > max) max = absSample;
+            }
+
+            progressBarAudioLevel.Invoke(new Action(() =>
+            {
+                progressBarAudioLevel.Value = Math.Min(max, progressBarAudioLevel.Maximum);
+            }));
+        }
+
+        private void WaveIn_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (waveIn != null)
+            {
+                waveIn.Dispose();
+                waveIn = null;
+                progressBarAudioLevel.Invoke(new Action(() =>
+                {
+                    progressBarAudioLevel.Value = 0;
+                }));
+            }
+        }
+        private string GenerateFileName()
+        {
+            string directory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Recordings");
+            Directory.CreateDirectory(directory); // Ensure the directory exists
+
+            string fileName;
+            do
+            {
+                fileName = Path.Combine(directory, $"Recording_{++fileCount}.wav");
+            } while (File.Exists(fileName));
+
+            return fileName;
+        }
+        private void RunWhisperX(string filePath)
+        {
+            string outputDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "WhisperXOutputs");
+            Directory.CreateDirectory(outputDirectory); // Ensure the output directory exists
+
+            string command = $"activate whisperx && whisperx \"{filePath}\" --output_dir \"{outputDirectory}\"";
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C {command}",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            Process process = new Process()
+            {
+                StartInfo = startInfo
+            };
+
+            process.Start();
+            process.WaitForExit();
+
+            // After the process completes, find the latest text file in the output directory
+            var outputFile = new DirectoryInfo(outputDirectory)
+                .GetFiles("*.txt")
+                .OrderByDescending(f => f.LastWriteTime)
+                .FirstOrDefault();
+
+            if (outputFile != null)
+            {
+                // Read the contents of the latest file
+                string textOutputFile = File.ReadAllText(outputFile.FullName);
+
+                // Update the TextBox on the UI thread
+                this.Invoke(new Action(() =>
+                {
+                    textOutput.Text += textOutputFile + Environment.NewLine;
+                }));
+            }
+        }
+
+    }
+}
